173d5611131e9ecc3f3593070e78405756494319
aleksandar93s <a_sandev@yahoo.com>
2024-05-21 21:15:25 +0200

    first

---
+++ integrated_systems/EShop.Domain/DTO/MovieDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Domain.DTO
+{
+    public class MovieDTO
+    {
+        public string MovieName { get; set; }
+        public string MovieDescription { get; set; }
+        public string MovieImage { get; set; }
+        public double Rating { get; set; }
+    }
+}
---
+++ integrated_systems/EShop.Web/Controllers/API/AdminController.cs
@@ -0,0 +1,57 @@
+﻿using EShop.Domain.Domain;
+using EShop.Domain.DTO;
+using EShop.Service.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Movie_App.Service.Interface;
+
+namespace EShop.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        private readonly IMovieService _movieService;
+        public AdminController(IOrderService orderService, IMovieService movieService)
+        {
+            _orderService = orderService;
+            _movieService = movieService;
+        }
+
+        [HttpGet]
+        public List<Order> Index()
+        {
+            return _orderService.GetAllOrders();
+        }
+
+        [HttpGet("[action]")]
+        public List<Order> GetAllOrders()
+        {
+            return _orderService.GetAllOrders();
+        }
+
+        [HttpPost("[action]")]
+        public Order GetDetailsForOrder(BaseEntity model)
+        {
+            return _orderService.GetDetailsForOrder(model);
+        }
+        [HttpPost("[action]")]
+        public bool ImportAllMovies(List<MovieDTO> model)
+        {
+            bool status = true;
+            foreach (var movie in model)
+            {
+                var movie2 = new Movie
+                {
+                    MovieName = movie.MovieName,
+                    MovieDescription = movie.MovieDescription,
+                    MovieImage = movie.MovieImage,
+                    Rating = movie.Rating
+                };
+                _movieService.CreateNewMovie(movie2);
+            }
+            return status;
+        }
+    }
+}
